badd1388fa2172c697df8ad26bf0fe42ab3de6ea
sleepless-se <sleepless.se@gmail.com>
2019-06-18 06:36:50 +0800

    vott to yolo

--- voc_annotation.py
+++ voc_annotation.py
@@ -3,11 +3,11 @@ from os import getcwd
 
 sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["item"]
 
 
 def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id.replace(".jpg","")))
     tree=ET.parse(in_file)
     root = tree.getroot()
 
@@ -18,16 +18,22 @@ def convert_annotation(year, image_id, list_file):
             continue
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
-        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
+        b = (int(float(xmlbox.find('xmin').text)),
+             int(float(xmlbox.find('ymin').text)),
+             int(float(xmlbox.find('xmax').text)),
+             int(float(xmlbox.find('ymax').text)))
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
 
 wd = getcwd()
 
 for year, image_set in sets:
     image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+    list_file = open('model_data/%s_%s.txt'%(year, image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
+        if image_id == '1': continue
+        if image_id == '-1': continue
+        image_file_path = '%s/VOCdevkit/VOC%s/JPEGImages/%s'%(wd, year, image_id)
+        list_file.write(image_file_path)
         convert_annotation(year, image_id, list_file)
         list_file.write('\n')
     list_file.close()
