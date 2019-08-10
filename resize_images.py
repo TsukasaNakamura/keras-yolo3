bd5077b82f1fc92194a85ffa513a63355f3c6996
sleepless-se <sleepless.se@gmail.com>
2019-08-10 12:57:36 +0800

    fix image size bug

--- resize_images.py
+++ resize_images.py
@@ -9,27 +9,32 @@ def resize_images(images_dir, image_save_dir, image_size):
 
     os.makedirs(image_save_dir, exist_ok=True)
 
-    image_paths = glob(os.path.join(images_dir, '*'))
+    img_paths = glob(os.path.join(images_dir, '*'))
 
-    for img_path in image_paths:
+    for img_path in img_paths:
+        # resize
         image = Image.open(img_path)
-        image = image.convert('RGB')
-        image.thumbnail([image_size,image_size])
+        rgb_im = image.convert('RGB')
+        rgb_im.thumbnail([image_size,image_size])
+
+        # make background
         back_ground = Image.new("RGB", (image_size,image_size), color=(255,255,255))
-        back_ground.paste(image)
+        back_ground.paste(rgb_im)
+
+        # make path
         save_path = os.path.join(image_save_dir, os.path.basename(img_path))
         end_index = save_path.rfind('.')
         save_path = save_path[0:end_index]+'.jpg'
         print('save',save_path)
-        back_ground.save(save_path,'JPEG')
+        back_ground.save(save_path,quality=95,format='JPEG')
 
 
 def _main():
-    images_dir = 'image/'  # input directory
+    images_dir = 'images/'  # input directory
     image_save_dir = 'resize_image/'  # output directory
     image_size = 320
     if len(sys.argv) > 1:
-        input_shape = (int(sys.argv[1]),int(sys.argv[1]))
+        image_size = int(sys.argv[1])
 
     resize_images(images_dir=images_dir, image_save_dir=image_save_dir, image_size=image_size)
 
